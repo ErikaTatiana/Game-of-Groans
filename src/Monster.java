3bb25fdee2d9b694bc53c408dd85baca411a15e5
Erika Piltz <erikapiltzoliveiros@gmail.com>
2021-04-27 19:02:49 -0700

    reformating

--- src/Monster.java
+++ src/Monster.java
@@ -1,5 +1,4 @@
 import java.util.Random;
-//test change 2
 
 public class Monster {
     private int health;
@@ -11,9 +10,10 @@ public class Monster {
         this.damage = damage;
     }
 
-    public void setMHealth(int health){
-        this.health=health;
+    public void setMHealth(int health) {
+        this.health = health;
     }
+
     public void setMonsterType(String monsterType) {
         this.monsterType = monsterType;
     }
@@ -29,35 +29,37 @@ public class Monster {
     public String getMonsterType() {
         return monsterType;
     }
-    public void MonsterChoice(){
-        int rnum=r.nextInt(4);
-        if(rnum==0){
+
+    public void MonsterChoice() {
+        int rnum = r.nextInt(4);
+        if (rnum == 0) {
             this.monsterType = "Goblin";
             setMDamage(10);
             setMHealth(6);
-        }else if(rnum==1){
+        } else if (rnum == 1) {
             this.monsterType = "Zombie";
             setMDamage(15);
             setMHealth(12);
-        }else if(rnum==2){
+        } else if (rnum == 2) {
             this.monsterType = "Orc";
             setMDamage(20);
             setMHealth(18);
-        }else if(rnum==3){
+        } else if (rnum == 3) {
             this.monsterType = "Deneke";
             setMDamage(5);
             setMHealth(55);
         }
 
     }
+
     /* Hits the targeted Player */
     public void attack(Player target) {
-        target.setPHealth(target.getPHealth()-getMDamage());
+        target.setPHealth(target.getPHealth() - getMDamage());
     }
 
     /* Removes health from this Monster
     when hit by a Player */
     public void onHit(int damage) {
-        setMHealth(getMHealth()-damage);
+        setMHealth(getMHealth() - damage);
     }
 }
--- src/Player.java
+++ src/Player.java
@@ -11,50 +11,49 @@ public class Player {
     private Random r = new Random();
     private Scanner s = new Scanner(System.in);
 
-    //setters
-    public void setPHealth(int health) {
-        this.health = health;
-    }
-
-    public void setPGold(int gold) {
-        this.gold = gold;
-    }
-
-    public void setPDamage(int damage) {
-        this.damage = damage;
-    }
-
-    public void setPLootModifier(double lootModifier) {
-        this.lootModifier = lootModifier;
-    }
-
-    public void setPlayerClass(String playerClass) {
-        this.playerClass = playerClass;
-    }
-
     //getters
     public int getPHealth() {
         return this.health;
+    }
+
+    //setters
+    public void setPHealth(int health) {
+        this.health = health;
     }
 
     public int getPGold() {
         return this.gold;
     }
 
+    public void setPGold(int gold) {
+        this.gold = gold;
+    }
+
     public int getPDamage() {
         return this.damage;
+    }
+
+    public void setPDamage(int damage) {
+        this.damage = damage;
     }
 
     public double getPLootModifier() {
         return this.lootModifier;
     }
 
-    public String getPlayerClass(){
+    public void setPLootModifier(double lootModifier) {
+        this.lootModifier = lootModifier;
+    }
+
+    public String getPlayerClass() {
         return this.playerClass;
     }
 
+    public void setPlayerClass(String playerClass) {
+        this.playerClass = playerClass;
+    }
 
-    public void introScene(){
+    public void introScene() {
         System.out.println("=================================================");
         System.out.println("you are in a dungeon!!");
         System.out.println("There are monsters, bags of gold, and healing elixirs in each room.");
@@ -66,13 +65,13 @@ public class Player {
         int playerClass = s.nextInt();
         System.out.println("=================================================");
 
-        if (playerClass == 1){
+        if (playerClass == 1) {
             setPHealth(100);
             setPLootModifier(1.0);
             setPDamage(15);
             setPGold(0);
             setPlayerClass("Warrior");
-        }else if (playerClass ==2){
+        } else if (playerClass == 2) {
             setPHealth(70);
             setPLootModifier(1.2);
             setPDamage(10);
@@ -84,22 +83,22 @@ public class Player {
 
     /* Hits the targeted Monster */
     public void attack(Monster target) {
-        target.setMHealth(target.getMHealth()- getPDamage());
+        target.setMHealth(target.getMHealth() - getPDamage());
     }
 
     /* Removes health from this Player
     when hit by a Monster */
     public void onHit(int damage) {
-        setPHealth(getPHealth()-damage);
+        setPHealth(getPHealth() - damage);
     }
 
     /* Adds health to this Player when healed */
     public void onHeal(int health) {
-        setPHealth(getPHealth()+health);
+        setPHealth(getPHealth() + health);
     }
 
     /* Adds gold to this Player when obtained */
     public void onLoot(int gold) {
-        setPGold((int)getPLootModifier()*r.nextInt());
+        setPGold((int) getPLootModifier() * r.nextInt());
     }
 }
