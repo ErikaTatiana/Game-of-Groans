53d1ad245d696e46afb954cc674040255346498a
Erika Piltz <erikapiltzoliveiros@gmail.com>
2021-04-28 00:36:49 -0700

    adding Thors additions

--- src/DungeonMap.java
+++ src/DungeonMap.java
@@ -1,23 +1,84 @@
-//Done by Thunderman
-/* Contains the Rooms in the dungeon 
-and logic for Player movement */
+/*Contains the Rooms in the dungeon
+        and logic for Player movement **/
 public class DungeonMap {
-	/* Rooms in the dungeon */
+    /* Rooms in the dungeon */
     private Room[][] rooms;
+    private int rows;
+    private int columns;
+    private int playerXPos = 0;
+    private int playerYPos = 0;
 
     /* Reference to the Player in the dungeon */
     private Player player;
 
+    public int getPlayerXPos() {
+        return this.playerXPos;
+    }
+    public int getPlayerYPos() {
+        return this.playerYPos;
+    }
+    public void setPlayerXPos(int xPos) {
+        this.playerXPos = xPos;
+    }
+    public void setPlayerYPos(int yPos) {
+        this.playerYPos = yPos;
+    }
+
     /* Initializes the rooms and shared Player reference */
     public DungeonMap (int rows, int columns, Player player){
-
+        rooms = new Room[rows][columns];
+        this.rows = rows;
+        this.columns = columns;
+        this.player = player;
+        // New rooms for each row and column
+        for(int r = 0; r < rows; r++) {
+            for(int c = 0; c < columns; c++) {
+                rooms[r][c] = new Room((this.rows)*(this.columns));
+            }
+        }
+        //Does the room we add the player in start a new encounter or is that marked as visited?
     }
 
     /* Displays the dungeon's rooms, walls,
     and player's current location */
     public void print() {
 
+        //Top Walls
+        System.out.print("+");
+        for(int i = 0; i < this.rows; i++){
+            System.out.print("-");
+        }
+        System.out.println("+");
+
+        //Side walls and Rooms
+        for(int r = 0; r < this.rows; r++) {
+            System.out.print("|");
+            for(int c = 0; c < this.columns; c++) {
+                if (rooms[r][c].hasVisited()) {
+                    //Visited room
+                    System.out.print("*");
+                }
+                else if(r == this.playerXPos && c == this.playerYPos) {
+                    // Player position
+                    System.out.print(player.getPlayerClass());
+                    // Not sure if the getClass method is going to give the symbol or the name of the class
+                }
+                else {
+                    //unvisited room
+                    System.out.print(" ");
+                }
+            }
+            System.out.println("|");
+        }
+
+        // Bottom Walls
+        System.out.print("+");
+        for(int i = 0; i < this.rows; i++){
+            System.out.print("-");
+        }
+        System.out.println("+");
+
     }
-	
-	//TODO: method(s) to move player
+
+    //TODO: method(s) to move player
 }
--- src/Room.java
+++ src/Room.java
@@ -7,13 +7,88 @@ public class Room {
     /* Indicates whether or not this Room
     instance has been visited already */
     private boolean visited;
+    private final int avgGoldperRoom;
 
-	/* Handles encounter logic when a Player
-	enters this Room. Includes combat resolution
-	and obtaining loot. */
+    public Room(int totalRooms) {
+        this.visited = false;
+        //This is how much gold per room will get you 100 gold if you get gold in 1/5 of the rooms
+        this.avgGoldperRoom = (100/(totalRooms/5));
+    }
+
+    /* Handles encounter logic when a Player
+    enters this Room. Includes combat resolution
+    and obtaining loot. */
     public void enter(Player player) {
+        System.out.println("You open a door and move to the next room...");
+        if(!this.visited) {
+            String encounter = randomEncounter();
+            if(encounter.equals("Monster")) {
+                Monster monster = new Monster();
+                System.out.println("A " + monster.getMonsterType() + " has appeared!");
+                fight(player, monster);
+            }
+            else if(encounter.equals("Loot")) {
+                System.out.println("You have found gold!");
+                Random random = new Random();
+                int goldDeviation = 3 - random.nextInt(6);
+                player.onLoot(goldDeviation + this.avgGoldperRoom);
+                System.out.println("You found " + goldDeviation + this.avgGoldperRoom + " gold. This gives you a total of " + player.getGold() + " gold!");
+            }
+            else {
+                System.out.println("You have found a healing elixir! ");
+                Random random = new Random();
+                player.onHeal(30);
+                System.out.println("You have gained 30 health! Your health is now: " + player.getHealth());
+            }
+        }
+        else {
+            System.out.print("You have visited this room before.");
+        }
 
     }
+
+    // Setup for new Rooms
+    public String randomEncounter()  {
+        Random random = new Random();
+        int randomNumber = random.nextInt(1);
+        if(randomNumber == 0) {
+            return "Monster";
+        }
+        else {
+            randomNumber = random.nextInt(1);
+            if (randomNumber == 0) {
+                return "Loot";
+            } else {
+                return "Healing";
+            }
+        }
+    }
+
+    // Fight sequence
+    public void fight(Player player, Monster monster) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Would you like to run or fight the " + monster.getMonsterType() + " ? ([R] or [F])");
+        String decision = scanner.nextLine();
+        if(decision.equalsIgnoreCase("r")) {
+            monster.attack(player);
+        }
+        else if(decision.equalsIgnoreCase("f")) {
+            //fight sequence
+            if(player.getPHealth() > 0 && monster.getMHealth() > 0) {
+                monster.attack(player);
+                System.out.println("----------");
+                if(player.getPHealth() > 0) {
+                    player.attack(monster);
+                    fight(player, monster);
+                }
+            }
+        }
+        else {
+            System.out.print("Input not recognized, please try again.");
+            this.fight(player, monster);
+        }
+    }
+
 
     /* Accessor for the visited field */
     public boolean hasVisited() {
